815880d1b672ec812d181eac89b01dc422e394e3
Flor Araceli <al222111334@gmail.com>
2024-06-29 10:44:22 -0600

    mi api

---
+++ src/main/java/edu/utvt/attendance/persistence/entities/Student.java
@@ -0,0 +1,54 @@
+package edu.utvt.attendance.persistence.entities;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import edu.utvt.attendance.persistence.common.StudentType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "tc_students")
+public class Student {
+	
+	@Id
+	@Column(length = 12)
+	private String id;
+	
+	@Column(length = 50, nullable = false)
+	private String firstName;
+	
+	@Column(length = 100, nullable = false)
+	private String lastName;
+	
+	@Column(length = 50, nullable = false)
+	private String email;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private StudentType type;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	@Column(nullable = false, columnDefinition = "DATE")
+	private Date birthDate;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss" )
+	@Column(nullable = false, updatable = false)
+	@CreationTimestamp
+	private Date createdOn;
+	
+	
+}
---
+++ src/main/java/edu/utvt/attendance/persistence/service/StudentServiceImplementation.java
@@ -0,0 +1,92 @@
+package edu.utvt.attendance.persistence.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import edu.utvt.attendance.persistence.entities.Student;
+import edu.utvt.attendance.persistence.repositories.StudentRepository;
+
+@Service
+public class StudentServiceImplementation implements StudentService {
+	
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Override
+	public Student save(Student student) {
+		// TODO Auto-generated method stub
+		return this.studentRepository.save(student);
+	}
+
+	@Override
+	public Student update(String id,Student student) {
+		// TODO Auto-generated method stub
+		Optional<Student> studentOptional = null;
+		studentOptional = this.studentRepository.findById(id);
+		
+		if(studentOptional.isPresent()) {
+			
+			studentOptional.get().setId(student.getId());
+			studentOptional.get().setFirstName(student.getFirstName());
+			studentOptional.get().setLastName(student.getLastName());
+			studentOptional.get().setEmail(student.getEmail());
+			studentOptional.get().setType(student.getType());
+			studentOptional.get().setBirthDate(student.getBirthDate());
+			
+			this.studentRepository.save(studentOptional.get());
+		}
+		return studentOptional.orElse(student);
+	}
+
+	@Override
+	public List<Student> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Student> findById(String id) {
+		
+		return this.studentRepository.findById(id);
+		
+		
+	}
+	
+	
+	@Override
+	public Page<Student> get(Integer page, Integer size){
+		
+		PageRequest pageRequest = PageRequest.of(page, size, Sort.by("lastName"));
+		
+		return this.studentRepository.findAll(pageRequest);
+		
+	}
+	
+
+	@Override
+	public ResponseEntity<Student> deleteById (String id) {
+		// TODO Auto-generated method stub
+		Optional<Student> studentOptional =null;
+		ResponseEntity<Student> responseEntity = null;
+		
+		studentOptional = this.studentRepository.findById(id);
+		
+		if(studentOptional.isPresent()) {
+			
+			this.studentRepository.delete(studentOptional.get());
+			responseEntity = ResponseEntity.noContent().build();
+		} else {
+			responseEntity = ResponseEntity.notFound().build();
+		}
+		return responseEntity;
+		
+	}
+
+}
